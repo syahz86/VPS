ec40dff26ce2cbfcf4053b0ec1eae04dc6ab8bdc
Tony Esterdark <messi.klate@gmail.com>
2017-06-20 16:33:16 +0800

    Update CustomCentos-kvm.sh

--- CustomCentos-kvm.sh
+++ CustomCentos-kvm.sh
@@ -326,6 +326,6 @@ echo "Fail2Ban : [on]"
 echo "IPv6     : [off]"
 echo "Torrent Block :[on]" 
 echo "Playstation Block :[on]" 
-echo -e "Please type \e[34;1mmenu\e[0m for options"
+echo -e "Please type \e[1;33;44mmenu\e[0m for options"
 
 echo "================================================"
