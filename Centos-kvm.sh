db1f8b4182e1d26a70228cee65376a3c6e169ced
Tony Esterdark <messi.klate@gmail.com>
2017-08-30 05:31:31 +0800

    Update Centos-kvm.sh

--- Centos-kvm.sh
+++ Centos-kvm.sh
@@ -239,25 +239,6 @@ iptables-save
 
 #bonus block torrent
 iptables -A INPUT -m string --algo bm --string "BitTorrent" -j REJECT
-iptables -A INPUT -m string --algo bm --string "BitTorrent protocol" -j REJECT
-iptables -A INPUT -m string --algo bm --string "peer_id=" -j REJECT
-iptables -A INPUT -m string --algo bm --string ".torrent" -j REJECT
-iptables -A INPUT -m string --algo bm --string "announce.php?passkey=" -j REJECT
-iptables -A INPUT -m string --algo bm --string "torrent" -j REJECT
-iptables -A INPUT -m string --algo bm --string "info_hash" -j REJECT
-iptables -A INPUT -m string --algo bm --string "/default.ida?" -j REJECT
-iptables -A INPUT -m string --algo bm --string ".exe?/c+dir" -j REJECT
-iptables -A INPUT -m string --algo bm --string ".exe?/c_tftp" -j REJECT
-iptables -A INPUT -m string --string "peer_id" --algo kmp -j REJECT
-iptables -A INPUT -m string --string "BitTorrent" --algo kmp -j REJECT
-iptables -A INPUT -m string --string "BitTorrent protocol" --algo kmp -j REJECT
-iptables -A INPUT -m string --string "bittorrent-announce" --algo kmp -j REJECT
-iptables -A INPUT -m string --string "find_node" --algo kmp -j REJECT
-iptables -A INPUT -m string --string "info_hash" --algo kmp -j REJECT
-iptables -A INPUT -m string --string "get_peers" --algo kmp -j REJECT
-iptables -A INPUT -p tcp --dport 25 -j REJECT   
-iptables -A FORWARD -p tcp --dport 25 -j REJECT 
-iptables -A OUTPUT -p tcp --dport 25 -j REJECT 
 iptables-save
 
 # install speedtest server
